3e669cc59ccb260126a02d06ac1c02086ed4babc
alohaisai <mm70aloha@gmail.com>
2019-07-30 13:59:26 +0900

    first commit index.htmlとapp.jsをプッシュします

---
+++ app.js
@@ -0,0 +1,23 @@
+let array = [
+  ["apple","りんご"],
+  ["orange","みかん"]
+];
+
+console.log(array[0]);
+console.log(array[0][1]);
+
+console.log(array[1][1]);
+
+array[0][1]="バナナ";
+console.log(array[0]);
+
+array[1][0]="banana";
+console.log(array[1]);
+
+
+array = [
+  ["pink","ピンク"],
+  ["red","赤"]
+];
+
+console.log(array[0]);
